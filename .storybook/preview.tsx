9be9b157e4e938daa3835c42d564b6ca948e95bb
trishonbaidaya7399 <githubtrishon@gmail.com>
2025-09-09 21:54:22 +0600

    story book setup is done

---
+++ .storybook/preview.tsx
@@ -0,0 +1,24 @@
+import type { Preview } from "@storybook/nextjs-vite";
+import { withThemeFromJSXProvider } from "@storybook/addon-themes";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {},
+    defaultTheme: "light",
+   Provider: ({ children }) => <>{children}</>,
+  }),
+];
+
+export default preview;
